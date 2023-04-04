c6d1eb998163393358e3dcf051fc3f1b27fcf5df
Horiguchi Takahiro <horiguchitakahiro@Horiguchis-MacBook-Pro.local>
2023-04-04 16:45:12 +0900

    firebase added

---
+++ competitive_virtual/src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from 'firebase/storage';
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+};
+console.log(firebaseConfig.apiKey);
+console.log(process.env.REACT_APP_FIREBASE_API_KEY)
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore(app);
+export const storage = getStorage();
+
