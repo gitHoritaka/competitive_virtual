c47c39c8c767247c9e3831e7323a28b3fb6f1b3c
Horiguchi Takahiro <horiguchitakahiro@Horiguchis-MacBook-Pro.local>
2023-04-07 06:57:45 +0900

    add problems

--- competitive_virtual/src/components/AddProblem.js
+++ competitive_virtual/src/components/AddProblem.js
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const AddProblem = () => {
+  const HandleChangeURL = (event) =>{
+  };
+  const HandleChangeTitle = (event) => {
+  };
+  const HandleSubmit = (event) =>{
+    console.log("submit");
+  };
   return (
-    <div>AddProblem</div>
+    <div>
+      <h1>add problem</h1>
+    <form onSubmit = {HandleSubmit}>
+        <div>
+            <label>input url</label>
+            <input name = "problem_url" type="url" placeholder = "add url" onChange={(event) => HandleChangeURL(event)}  />
+        </div>
+        <div>
+            <label>title</label>
+            <input name = 'problem_title' type = 'text' placeholder="title" onChange={(event) => HandleChangeTitle(event)} ></input>
+        </div>
+        <div>
+            <button tyoe = 'submit'>register</button>
+        </div>
+
+    </form> 
+    </div>
   )
 }
 
