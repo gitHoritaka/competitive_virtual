8c7fadc786a65d2edb8712b9adc26361e96c05a3
nullegg <mebookbow@outlook.jp>
2023-05-06 13:37:49 +0900

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-/*
 import { initializeApp } from 'firebase/app';
 import { getAuth,GoogleAuthProvider } from 'firebase/auth';
 import {getFirestore} from "firebase/firestore";
@@ -12,32 +11,6 @@ const firebaseConfig = {
   collection_name:process.env.REACT_APP_FIREBASE_COLLECTION_NAME,
 };
 //
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-const db = getFirestore(app);
-const clname = firebaseConfig.collection_name
-export {app,auth,provider,db,clname}
-*/
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCIEU6LW4wFbStDN8VVKROkx1R2CBVXCyo",
-  authDomain: "blog-a48ef.firebaseapp.com",
-  projectId: "blog-a48ef",
-  storageBucket: "blog-a48ef.appspot.com",
-  messagingSenderId: "686096451956",
-  appId: "1:686096451956:web:0ecb83145e1bda21720421"
-};
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
